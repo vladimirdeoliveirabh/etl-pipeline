8e4648ac6376890a041223509d2c628e1eab78b6
Vladimir de Oliveira <vladimirdeoliveirabh@gmail.com>
2025-01-21 17:43:39 -0300

    feat: HttpRequester

---
+++ src/drivers/http_requester.py
@@ -0,0 +1,13 @@
+import requests
+
+class HttpRequester:
+
+    def __init__(self) -> None:
+        self.__url = 'http://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm'
+
+    def request_from_page(self) -> dict[int, str]:
+        response = requests.get(self.__url, timeout=10)
+        return {
+            "status_code": response.status_code,
+            "html": response.text
+        }
