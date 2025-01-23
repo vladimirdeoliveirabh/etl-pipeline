9bfdac673ff0a205817719aeff65970c064f9eba
Vladimir de Oliveira <vladimirdeoliveirabh@gmail.com>
2025-01-22 21:54:01 -0300

    feat: Finishing implementing transform stage

--- src/errors/transform_error.py
+++ src/errors/transform_error.py
@@ -1,6 +1,6 @@
-class ExtractError(Exception):
+class TransformError(Exception):
 
     def __init__(self, message: str) -> None:
         super().__init__(message)
         self.message = message
-        self.error_type = 'Extract Error'
+        self.error_type = 'Transform Error'
--- src/stages/transform/transform_raw_data.py
+++ src/stages/transform/transform_raw_data.py
@@ -2,12 +2,20 @@
 
 from typing import List, Dict
 from src.stages.contracts.extract_contract import ExtractContract
+from src.stages.contracts.transform_contract import TransformContract
+from src.errors.transform_error import TransformError
 
 class TransformRawData:
 
-    def transform(self, extract_contract: ExtractContract):
-        transformed_information = self.__filter_and_transform_data(extract_contract)
-        return transformed_information
+    def transform(self, extract_contract: ExtractContract) -> TransformContract:
+        try:
+            transformed_information = self.__filter_and_transform_data(extract_contract)
+            transformed_data_contract = TransformContract(
+                load_content=transformed_information
+            )
+            return transformed_data_contract
+        except Exception as exception:
+            raise TransformError(str(exception)) from exception
 
     def __filter_and_transform_data(self, extract_contract: ExtractContract) -> List[Dict]:
         extraction_date = extract_contract.extraction_date
